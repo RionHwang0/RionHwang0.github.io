ea6412ea231157274744b53b705c002c5e8a0b24
황현준 <rionhwang@gmail.com>
2024-09-16 23:46:09 +0900

    chore(deps): bump dev-deps with 7 updates
    chore(deps): bump dev-deps with 7 updates

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-
-// Define a basic task
-gulp.task('build', function(done) {
-  console.log('Building the project...');
-  // Add your build tasks here
-  done();
-});
